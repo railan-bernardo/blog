65a9a8a3f2cc68eea4eb4f3f33e61389dc640643
Railan Bernardo <railabernardo2016@gmail.com>
2021-07-19 10:58:37 -0300

    blog

---
+++ vendor/composer/installed.php
@@ -0,0 +1,123 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'upinside/fsphp',
+  ),
+  'versions' => 
+  array (
+    'coffeecode/cropper' => 
+    array (
+      'pretty_version' => '1.3.4',
+      'version' => '1.3.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4c98fa8601ad1dcb3d94a605e7bfa36b7ac5b87e',
+    ),
+    'coffeecode/optimizer' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '23d3a65cc8210ce6febec4701a5108cdf8b4674b',
+    ),
+    'coffeecode/paginator' => 
+    array (
+      'pretty_version' => '1.0.7',
+      'version' => '1.0.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a6941246f7ccd842be2072d796d4fb448c22cdb1',
+    ),
+    'coffeecode/router' => 
+    array (
+      'pretty_version' => '1.0.8',
+      'version' => '1.0.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '29f73e12351a116e9bbb3f695595777a09e18077',
+    ),
+    'coffeecode/uploader' => 
+    array (
+      'pretty_version' => '1.0.11',
+      'version' => '1.0.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '182682c029ff23d28aa8f5d7db2db4c060fc3ae4',
+    ),
+    'league/plates' => 
+    array (
+      'pretty_version' => 'v4.0.0-alpha',
+      'version' => '4.0.0.0-alpha',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f53f4c1fa4bf307d0f3858a348172ff4faf7669a',
+    ),
+    'matthiasmullie/minify' => 
+    array (
+      'pretty_version' => '1.3.66',
+      'version' => '1.3.66.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '45fd3b0f1dfa2c965857c6d4a470bea52adc31a6',
+    ),
+    'matthiasmullie/path-converter' => 
+    array (
+      'pretty_version' => '1.1.3',
+      'version' => '1.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e7d13b2c7e2f2268e1424aaed02085518afa02d9',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.5.0',
+      'version' => '6.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a5b5c43e50b7fba655f793ad27303cd74c57363c',
+    ),
+    'rosell-dk/image-mime-type-guesser' => 
+    array (
+      'pretty_version' => '0.3.1',
+      'version' => '0.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5915934d66a7869cecc7141adf90581aad81023d',
+    ),
+    'rosell-dk/webp-convert' => 
+    array (
+      'pretty_version' => '2.5.0',
+      'version' => '2.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b349da7b6e04a4c269bbbbf7c279e0e433a71fd0',
+    ),
+    'upinside/fsphp' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
